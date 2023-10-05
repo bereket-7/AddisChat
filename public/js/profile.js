efd386e1a51251c21b3a005f56eb69434cbc0f2c
bereket <bekhonelign@gmail.com>
2023-10-04 18:00:35 -0700

    profile js

---
+++ public/js/profile.js
@@ -0,0 +1,35 @@
+
+
+const express = require('express')
+const router = express.Router()
+const multer = require('multer')
+const isAuthenticated = require('../middleware/auth')
+
+// Configure multer for handling file uploads
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname)
+    },
+})
+const upload = multer({ storage })
+
+// Profile page route
+router.get('/profile', isAuthenticated, (req, res) => {
+    const user = req.user
+    res.render('profile', { user })
+})
+router.post('/profile', isAuthenticated, upload.single('profilePicture'), (req, res) => {
+    const user = req.user
+
+    if (req.file) {
+        user.profilePicture = '/uploads/' + req.file.filename
+    }
+    user.status = req.body.status
+
+    res.redirect('/profile')
+})
+
+module.exports = router
