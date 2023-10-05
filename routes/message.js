c96dfdf4144df1492367b588158ddd6d5e39ef05
bereket <bekhonelign@gmail.com>
2023-10-04 23:26:56 -0700

    message css ejs

--- routes/message.js
+++ routes/message.js
@@ -1,5 +1,17 @@
-const express = require('express')
-const router = express.Router()
-const isAuthenticated = require('../middleware/auth')
+const socket = io()
 
-module.exports = router
+const messageContainer = document.getElementById('message-container')
+const messageInput = document.getElementById('message-input')
+const sendButton = document.getElementById('send-button')
+
+socket.on('message', (message) => {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.appendChild(messageElement)
+})
+
+sendButton.addEventListener('click', () => {
+    const message = messageInput.value
+    socket.emit('sendMessage', message)
+    messageInput.value = ''
+})
