b49859fbf273379022cd4fb891c2bb5e6b85c223
bereket <bekhonelign@gmail.com>
2023-10-11 16:16:32 -0700

    search groups,chats implemetation

--- public/js/search.js
+++ public/js/search.js
@@ -1,18 +1,43 @@
+
 $(document).ready(function () {
     $('#search-form').submit(function (e) {
         e.preventDefault()
         const query = $('#search-input').val()
+
         searchUsers(query)
         searchGroups(query)
         searchChats(query)
     })
 
     function searchUsers(query) {
+
+        $.get(`/search/users?query=${query}`, function (data) {
+            const userResults = $('#user-list')
+            userResults.empty()
+            data.forEach(function (user) {
+                userResults.append(`<li>${user.username}</li>`)
+            })
+        })
     }
 
     function searchGroups(query) {
+        $.get(`/search/groups?query=${query}`, function (data) {
+            const groupResults = $('#group-list')
+            groupResults.empty()
+            data.forEach(function (group) {
+                groupResults.append(`<li>${group.name}</li>`)
+            })
+        })
     }
 
     function searchChats(query) {
+
+        $.get(`/search/chats?query=${query}`, function (data) {
+            const chatResults = $('#chat-list')
+            chatResults.empty()
+            data.forEach(function (chat) {
+                chatResults.append(`<li>${chat.name}</li>`)
+            })
+        })
     }
 })
