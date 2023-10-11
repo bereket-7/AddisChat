c41ab77df04f9eed565ed5eb78d729dbb36c8f1d
bereket <bekhonelign@gmail.com>
2023-10-11 16:02:48 -0700

    load selected group

--- public/js/group.js
+++ public/js/group.js
@@ -33,3 +33,53 @@ document.addEventListener('DOMContentLoaded', () => {
         chatMessages.appendChild(messageDiv)
     }
 })
+
+$(document).ready(function () {
+    $('#group-search').on('input', function () {
+        const searchText = $(this).val().toLowerCase()
+        const $searchResults = $('#search-results')
+        $searchResults.empty()
+
+        if (searchText) {
+            // Filter groups based on the search text and display matching results
+            const results = groups.filter((group) =>
+                group.name.toLowerCase().includes(searchText)
+            )
+
+            results.forEach((group) => {
+                $searchResults.append(
+                    `<li><a href="#" data-group="${group.id}">${group.name}</a></li>`
+                )
+            })
+        }
+    })
+    $('#search-results').on('click', 'a', function (e) {
+        e.preventDefault()
+        const groupId = $(this).data('group')
+
+        // Handle clicks on search results
+        $('#search-results').on('click', 'a', function (e) {
+            e.preventDefault()
+            const groupId = $(this).data('group')
+            loadGroup(groupId)
+        })
+
+        function loadGroup(groupId) {
+            $.ajax({
+                url: `/api/groups/${groupId}`,
+                method: 'GET',
+                success: function (data) {
+                    displayGroup(data)
+                },
+                error: function (error) {
+                    console.error('Error loading group:', error)
+                }
+            })
+        }
+
+        function displayGroup(groupData) {
+            $('#group-name').text(groupData.name)
+            $('#search-results').empty()
+        }
+    })
+})
